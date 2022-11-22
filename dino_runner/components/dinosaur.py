a9e676c4f7155506a689aae418285fcafecb7f02
Brenow7777 <breno.carvalho.silva.bcs@gmail.com>
2022-11-22 01:01:39 -0300

    Tarefa: dino_duck

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,6 +1,6 @@
 import pygame
 
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
@@ -16,6 +16,7 @@ class Dinosaur:
         self.jump_vel = JUMP_VEL
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
 
     def run(self):
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
@@ -34,11 +35,19 @@ class Dinosaur:
             self.dino_jump = False
             self.jump_vel = JUMP_VEL
 
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS + 35
+        self.step_index += 1
+
     def update(self, user_input):
         if self.dino_run:
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
 
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
@@ -46,6 +55,11 @@ class Dinosaur:
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
+
+        while user_input[pygame.K_DOWN]:
+            self.dino_duck = True
+            self.dino_run = False
+            break
 
         if self.step_index >= 10:
             self.step_index = 0
