d9342eebb347736f20ffd01cbd0bdaab9d035eba
Brenow7777 <breno.carvalho.silva.bcs@gmail.com>
2022-11-22 21:24:24 -0300

    Tarefa aula 2: large cactus and bird

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,8 +1,10 @@
 import pygame
+import random
 
-from dino_runner.utils.constants import SMALL_CACTUS
-from dino_runner.components.obstacles.cactus import Cactus
-
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
+from dino_runner.components.obstacles.smallcactus import SmallCactus
+from dino_runner.components.obstacles.largecactus import LargeCactus
+from dino_runner.components.obstacles.bird import Bird
 
 class ObstacleManager:
     def __init__(self):
@@ -10,7 +12,12 @@ class ObstacleManager:
 
     def update(self, game_speed, player):
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
+            if random.randint(0, 3) == 0:
+                self.obstacles.append(SmallCactus(SMALL_CACTUS))
+            elif random.randint(0, 3) == 1:
+                self.obstacles.append(LargeCactus(LARGE_CACTUS))
+            elif random.randint(0, 3) == 2 or random.randint == 3:
+                self.obstacles.append(Bird(BIRD))
 
         for obstacle in self.obstacles:
             obstacle.update(game_speed, self.obstacles)
